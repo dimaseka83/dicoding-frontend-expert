5f6e1d712dd62c0b79c96a32ce7b9f300d77ee07
dhimas <dimaseka83@gmail.com>
2022-08-17 21:46:38 +0700

    first

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/style.css'
+import '../styles/main.css';
+import '../styles/reset.min.css';
+
+console.log('Hello Coders! :)');
+// Headers
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
