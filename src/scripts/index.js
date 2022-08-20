c5c4a1807a4c9b21633e7e8bc098e2dac13f8f51
dhimas <dimaseka83@gmail.com>
2022-08-20 19:04:27 +0700

    ++

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,46 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/style.css'
 import '../styles/main.css';
-import '../styles/reset.min.css';
+import '../styles/responsive.css'
+import('../DATA.json').then(({ default: jsonData }) => {
+  let datas = jsonData['restaurants']
+  let dataList = '';
+  datas.forEach(function (data) {
+    dataList += `
+      <div class="card">
+    <div class="card-header">
+      <img src="${data['pictureId']}" alt="${data['name']}" title="${data['name']}"/>
+    </div>
+    <div class="card-body">
+      <span class="tag tag-teal">${data['city']}</span>
+      <h4>${data['name']}</h4>
+      <p>${data['description'].slice(0, 150)}...</p>
+      <div class="user">
+        <div class="user-info">
+          <h5>Rating ${data['rating']}</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+  });
+  document.querySelector('.container').innerHTML = dataList;
+})
 
-console.log('Hello Coders! :)');
-// Headers
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
+// Menu
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
 
-navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
-  }
-});
+menu.addEventListener('click', function (event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+})
+
+hero.addEventListener('click', function () {
+  drawer.classList.remove('open');
+})
+
+main.addEventListener('click', function () {
+  drawer.classList.remove('open');
+})
