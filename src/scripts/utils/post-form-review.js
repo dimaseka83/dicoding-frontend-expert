dac46520d682dc2b907930260e37cb6366e22091
dhimaseka <dimaseka83@gmail.com>
2022-08-30 14:26:07 +0700

    ++

--- src/scripts/utils/post-form-review.js
+++ src/scripts/utils/post-form-review.js
@@ -10,9 +10,20 @@ const PostFormReview = async (id, name, review) => {
         name,
         review,
     };
+
+    const reviewContainer = document.querySelector(".restaurant__customer-reviews__list");
+
+    const newReview = `
+    <div class="restaurant__customer-reviews__item">
+        <p class="restaurant__customer-reviews__item__name">${name}</p>
+        <p class="restaurant__customer-reviews__item__date">${date}</p>
+        <p class="restaurant__customer-reviews__item__review">${review}</p>
+    </div>
+    `
     try {
         const response = await RestaurantSource.postReview(data);
         initSuccess(response.message);
+        reviewContainer.innerHTML += newReview;
     } catch (error) {
         initError(error.message);
     }
